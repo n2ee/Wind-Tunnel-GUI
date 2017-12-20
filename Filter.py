b8a78b05923fe34b934d16f6efffe7223f6f4ab0
markm <markm@xmission.com>
2017-12-20 15:06:59 -0800

    Variable name changes, added variance computation
    Renamed some Kalman filter vars to be a little clear as to what's going on (for non-latin speakers).
    
    Add variance computation for RollingAverageFilter.

--- Filter.py
+++ Filter.py
@@ -16,6 +16,7 @@ A 1d filter suitable for smoothing out the data from the load cells.
 """
 
 from array import array
+from math import sin
 
 
 class KalmanFilter(object):
@@ -29,42 +30,53 @@ class KalmanFilter(object):
         self.process_variance = process_variance
         self.estimated_measurement_variance = \
                                 estimated_measurement_variance
-        self.posteri_estimate = 0.0
-        self.posteri_error_estimate = 1.0
+        self.current_estimate = 0.0
+        self.currenti_error_estimate = 1.0
 
     def get_filtered_value(self, measurement):
-        priori_estimate = self.posteri_estimate
-        priori_error_estimate = self.posteri_error_estimate + self.process_variance
+        previous_estimate = self.current_estimate
+        previous_error_estimate = self.currenti_error_estimate + \
+                                                        self.process_variance
 
-        blending_factor = priori_error_estimate / (priori_error_estimate + self.estimated_measurement_variance)
-        self.posteri_estimate = priori_estimate + blending_factor * (measurement - priori_estimate)
-        self.posteri_error_estimate = (1 - blending_factor) * priori_error_estimate
-        return self.posteri_estimate
+        blending_factor = previous_error_estimate / \
+                (previous_error_estimate + self.estimated_measurement_variance)
+        self.current_estimate = previous_estimate + \
+                            blending_factor * (measurement - previous_estimate)
+        self.currenti_error_estimate = (1 - blending_factor) * \
+                                                        previous_error_estimate
+        return self.current_estimate
 
 class RollingAverageFilter(object):
     
-    def __init__(self, windowSize = 30, average = 0.0, variance = 0.0):
+    def __init__(self, windowSize = 30, mean = 0.0, variance = 0.0):
         self.oldIndex = 0
-        self.oldMeasurement = array('f', (0.0 for i in range(0, windowSize)))
-        self.average = average
+        self.oldX = array('f', (0.0 for i in range(0, windowSize)))
+        self.mean = mean
+        self.variance = variance
         self.N = windowSize
         
-    def get_filtered_value(self, measurement):
-        self.average = (measurement + sum(self.oldMeasurement)) / self.N
-        self.oldMeasurement[self.oldIndex] = measurement
+    def get_filtered_value(self, x):
+        self.oldX[self.oldIndex] = x
         self.oldIndex += 1
-        self.oldIndex %= len(self.oldMeasurement)
-        
-        return self.average
+        self.oldIndex %= self.N    
+        self.mean = sum(self.oldX) / self.N
+        return self.mean
     
     def get_variance(self):
+        sum2 = 0.0
+        for i in range(0, self.N):
+            sum2 += (self.oldX[i] - self.mean) * (self.oldX[i] - self.mean)
+            
+        self.variance = sum2 / (self.N - 1)
+        
+
         return self.variance
 
 if __name__ == "__main__":
     import random
     iteration_count = 500
 
-    actual_values = [-0.37727 + j * j * 0.00001 for j in 
+    actual_values = [(-0.37727 + j * j * 0.00001) + 0.5 * sin(j/2.0) for j in 
                      range(iteration_count)]
     noisy_measurement = [random.random() * 2.0 - 1.0 + actual_val 
                          for actual_val in actual_values]
@@ -77,24 +89,27 @@ if __name__ == "__main__":
                    for j in range(iteration_count)])
 
     filter = RollingAverageFilter()
-    posteri_estimate_graph = []
+    current_estimate_graph = []
+    current_variance_graph = []
 
     for iteration in range(1, iteration_count):
         result = filter.get_filtered_value(noisy_measurement[iteration])
-        posteri_estimate_graph.append(result)
+        current_estimate_graph.append(result)
+        current_variance_graph.append(filter.get_variance())
 
     import pylab
     pylab.figure()
-    pylab.plot(noisy_measurement, color='r', label='noisy measurements')
-    pylab.plot(posteri_estimate_graph, 'b-', label='a posteri estimate')
     pylab.plot(actual_values, color='g', label='truth value')
+    pylab.plot(noisy_measurement, color='r', label='noisy measurements')
+    pylab.plot(current_estimate_graph, 'b-', label='filtered results')
+    pylab.plot(current_variance_graph, 'c', label='filtered variance')
     pylab.legend()
-    pylab.xlabel('Rolling Average Iteration')
+    pylab.xlabel('Rolling Average Filter')
     pylab.ylabel('Voltage')
     pylab.show()
     
-    actual_values = [-0.37727 + j * j * 0.00001 for j in 
-                     range(iteration_count)]
+    # actual_values = [-0.37727 + j * j * 0.00001 for j in 
+    #                  range(iteration_count)]
     noisy_measurement = [random.random() * 2.0 - 1.0 + actual_val 
                          for actual_val in actual_values]
 
@@ -110,18 +125,18 @@ if __name__ == "__main__":
     estimated_measurement_variance = measurement_standard_deviation ** 2  # 0.05 ** 2
     kalman_filter = KalmanFilter(process_variance, 
                                  estimated_measurement_variance)
-    posteri_estimate_graph = []
+    current_estimate_graph = []
 
     for iteration in range(1, iteration_count):
         result = kalman_filter.get_filtered_value(noisy_measurement[iteration])
-        posteri_estimate_graph.append(result)
+        current_estimate_graph.append(result)
 
     pylab.figure()
-    pylab.plot(noisy_measurement, color='r', label='noisy measurements')
-    pylab.plot(posteri_estimate_graph, 'b-', label='a posteri estimate')
     pylab.plot(actual_values, color='g', label='truth value')
+    pylab.plot(noisy_measurement, color='r', label='noisy measurements')
+    pylab.plot(current_estimate_graph, 'b-', label='filtered results')
     pylab.legend()
-    pylab.xlabel('Kalman Iteration')
+    pylab.xlabel('Kalman Filter')
     pylab.ylabel('Voltage')
     pylab.show()
     
