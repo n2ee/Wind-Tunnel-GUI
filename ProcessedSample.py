6858a74eecc1df2a95f393f675b7cba598950db7
markm <markm@xmission.com>
2017-12-21 18:20:57 -0800

    Removed hotwire field

--- ProcessedSample.py
+++ ProcessedSample.py
@@ -10,14 +10,13 @@ import datetime
 class ProcessedSample(object):
 
     def __init__(self, volts = 0.0, amps = 0.0, aoa = 0.0, rpm = 0, 
-                 airspeed = 0.0, hotwire = 0.0, lift = 0.0, drag = 0.0,
+                 airspeed = 0.0, lift = 0.0, drag = 0.0,
                  pitchmoment = 0.0, timestamp = datetime.datetime.now()):
         self.volts = volts
         self.amps = amps
         self.aoa = aoa
         self.rpm = rpm
         self.airspeed = airspeed
-        self.hotwire = hotwire
         self.lift = lift
         self.drag = drag
         self.pitchMoment = pitchmoment
@@ -25,10 +24,9 @@ class ProcessedSample(object):
 
     def __repr__(self):
         return "volts: %f, amps: %f, aoa: %f, rpm: %d, airspeed: %f, " \
-                "hotwire: %f, lift: %f, drag: %f, pitchMoment: %f, timestamp: %s" % \
+                "lift: %f, drag: %f, pitchMoment: %f, timestamp: %s" % \
             (self.volts, self.amps, self.aoa, self.rpm, self.airspeed,
-             self.hotwire, self.lift, self.drag, self.pitchMoment,
-             self.timestamp)        
+             self.lift, self.drag, self.pitchMoment, self.timestamp)        
         
 if __name__ == "__main__":
     testSample = [ProcessedSample(i / 10.0, i / 10.0, i / 10.0, i, i/ 10.0, \
