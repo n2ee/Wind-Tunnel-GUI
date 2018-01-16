9194fa4683a1ef7b4cf381636e7133c7b7627a66
markm <markm@xmission.com>
2018-01-16 14:36:08 -0800

    Add additional fields to object.
    Added standard deviation values to the objet, so an instance of it can be used to pass via signal to the TunnelWindow thread.

--- ProcessedSample.py
+++ ProcessedSample.py
@@ -10,10 +10,13 @@ import datetime
 class ProcessedSample(object):
 
     def __init__(self, volts = 0.0, amps = 0.0, aoa = 0.0, rpm = 0, 
-                 airspeed = 0.0, hotwire = 0.0,  liftLeft = 0.0, 
-                 liftCenter = 0.0, liftRight = 0.0, totalLift = 0.0, 
-                 drag = 0.0, pitchmoment = 0.0, 
+                 airspeed = 0.0, hotwire = 0.0, 
+                 liftLeft = 0.0, liftCenter = 0.0, liftRight = 0.0, 
+                 totalLift = 0.0, totalLiftStdDev = 0.0, 
+                 drag = 0.0, dragStdDev = 0.0, pitchmoment = 0.0,
+                 pitchMomentStdDev = 0.0,
                  timestamp = datetime.datetime.now()):
+        
         self.volts = volts
         self.amps = amps
         self.aoa = aoa
@@ -24,8 +27,11 @@ class ProcessedSample(object):
         self.liftCenter = liftCenter
         self.liftRight = liftRight
         self.totalLift = totalLift
+        self.totalLiftStdDev = totalLiftStdDev
         self.drag = drag
+        self.dragStdDev = dragStdDev
         self.pitchMoment = pitchmoment
+        self.pitchMomentStdDev = pitchMomentStdDev
         self.timestamp = timestamp
 
     """
@@ -36,21 +42,24 @@ class ProcessedSample(object):
     @staticmethod
     def header():
         return "Volts, Amps, Aoa, RPM, Airspeed, Hotwire, " \
-               "LiftLeft, LiftCenter, LiftRight, TotalLift, " \
-               "Drag, PitchMoment, Timestamp"
+               "LiftLeft, LiftCenter, LiftRight, TotalLift, TotalLiftStdDev, " \
+               "Drag, DragStdDev, PitchMoment, PitchMomentStdDev, Timestamp"
                
     def __repr__(self):
         return "%f, %f, %f, %d, %f, %f, " \
-                "%f, %f, %f, %f, " \
-                "%f, %f, %s" % \
+                "%f, %f, %f, %f, %f, " \
+                "%f, %f, %f, %f, %s" % \
             (self.volts, self.amps, self.aoa, self.rpm, self.airspeed,
              self.hotwire, self.liftLeft, self.liftCenter, self.liftRight, 
-             self.totalLift, self.drag, self.pitchMoment, self.timestamp)        
+             self.totalLift, self.totalLiftStdDev, self.drag, 
+             self.dragStdDev, self.pitchMoment, self.pitchMomentStdDev, 
+             self.timestamp)        
         
 if __name__ == "__main__":
     testSample = [ProcessedSample(i / 10.0, i / 10.0, i / 10.0, i, i/ 10.0, \
                                   i/ 10.0, i / 10.0, i / 10.0, i / 10.0, \
-                                  i / 10.0, i / 10.0, i / 10.0, i) \
+                                  i / 10.0, i / 10.0, i / 10.0, i / 10.0, \
+                                  i / 10.0, i / 10.0, i) \
                     for i in range(10)]
 
     print (ProcessedSample.header())
