3aca9ae8939d01729510c28dceb48b0c790e2cac
markm <markm@xmission.com>
2018-02-14 12:57:29 -0800

    Add raw AoA, raw airspeed
    Pass the raw AoA, raw airspeed values back to the main window so they can be seen in the calibrate dialog.

--- ProcessedSample.py
+++ ProcessedSample.py
@@ -10,7 +10,8 @@ import datetime
 class ProcessedSample(object):
 
     def __init__(self, volts = 0.0, amps = 0.0, watts = 0.0,
-                 aoa = 0.0,airspeed = 0.0, hotwire = 0.0, 
+                 rawAoa = 0.0, aoa = 0.0,
+                 rawAirspeed = 0.0, airspeed = 0.0, hotwire = 0.0, 
                  liftLeft = 0.0, liftCenter = 0.0, liftRight = 0.0, 
                  totalLift = 0.0, totalLiftStdDev = 0.0, 
                  drag = 0.0, dragStdDev = 0.0, pitchmoment = 0.0,
@@ -20,7 +21,9 @@ class ProcessedSample(object):
         self.volts = volts
         self.amps = amps
         self.watts = watts
+        self.rawAoa = rawAoa
         self.aoa = aoa
+        self.rawAirspeed = rawAirspeed
         self.airspeed = airspeed
         self.hotwire = hotwire
         self.liftLeft = liftLeft
@@ -41,27 +44,31 @@ class ProcessedSample(object):
     """
     @staticmethod
     def header():
-        return "Volts, Amps, Watts, AoA Deg, Airspeed MPH, " \
+        return "Volts, Amps, Watts, Raw AoA, AoA Deg, Raw Airspeed, Airspeed MPH, " \
                "Hotwire MPH, LiftLeft Kg, LiftCenter Kg, LiftRight Kg, " \
                "TotalLift Kg, TotalLiftStdDev, " \
                "Drag Kg, DragStdDev, PitchMoment Kg-in, PitchMomentStdDev, " \
                "Timestamp"
                
     def __repr__(self):
-        return "%f, %f, %f, %f, %f, %f, " \
-                "%f, %f, %f, %f, %f, " \
-                "%f, %f, %f, %f, %s" % \
-            (self.volts, self.amps, self.watts, self.aoa, self.airspeed,
-             self.hotwire, self.liftLeft, self.liftCenter, self.liftRight, 
+        return "%f, %f, %f, %f, %f, " \
+                "%f, %f, %f, " \
+                "%f, %f, %f, " \
+                "%f, %f, %f, " \
+                "%f, %f, %f, %s" % \
+            (self.volts, self.amps, self.watts, self.rawAoa, self.aoa, 
+             self.rawAirspeed, self.airspeed, self.hotwire, 
+             self.liftLeft, self.liftCenter, self.liftRight, 
              self.totalLift, self.totalLiftStdDev, self.drag, 
              self.dragStdDev, self.pitchMoment, self.pitchMomentStdDev, 
              self.timestamp)        
         
 if __name__ == "__main__":
     testSample = [ProcessedSample(i / 10.0, i / 10.0, i / 10.0, i / 10.0, i/ 10.0, \
-                                  i/ 10.0, i / 10.0, i / 10.0, i / 10.0, \
-                                  i / 10.0, i / 10.0, i / 10.0, i / 10.0, \
-                                  i / 10.0, i / 10.0, i) \
+                                  i/ 10.0, i / 10.0, i / 10.0, \
+                                  i / 10.0, i / 1.0, i / 10.0, \
+                                  i / 10.0, i / 1.0, i / 10.0, \
+                                  i / 10.0, i / 10.0, i / 10.0, i) \
                     for i in range(10)]
 
     print (ProcessedSample.header())
