accc40e3633b07dd49f98257d5850ce73f5d3645
n2ee <32445403+n2ee@users.noreply.github.com>
2018-12-27 15:37:26 -0800

    Change hotwire calculations for new sensor

--- SampleCollector.py
+++ SampleCollector.py
@@ -186,7 +186,7 @@ class SampleCollector(QThread):
         pitchMomentFilter = RollingAverageFilter(10)
         dragFilter = RollingAverageFilter(10)
         airspeedFilter = RollingAverageFilter(10)
-        hotwireFilter = RollingAverageFilter(10)
+        hotwireFilter = RollingAverageFilter(20)
         wingAoA = 0
         platformAoA = 0
         
@@ -278,8 +278,15 @@ class SampleCollector(QThread):
                 airspeed = 0.0
 
             # Compute hotwire speed
-            hotwire = latestSample.hotwire
-            hotwire = hotwire * self.hotwireSlope + self.hotwireZero
+            hotwireCounts = latestSample.hotwire
+            hotwirePressure = (hotwireCounts - self.hotwireZero) / 1379.3
+            # hotwire = hotwire * self.hotwireSlope + self.hotwireZero
+            try:
+                hotwire = sqrt((hotwirePressure * self.hotwireSlope * 144.0  * 2.0) / 0.002378)
+            except ValueError:
+                # hotwirePressure went negative due to rounding errors
+                hotwire= 0.0
+
             if (hotwire < self.airspeedLowerLimit):
                 hotwire = 0.0
 
